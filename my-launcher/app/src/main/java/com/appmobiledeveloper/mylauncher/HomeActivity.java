f808048b5715403fa46373a36f67ee47c38955ab
Mobile Developer <app.mobile.developer@gmail.com>
2017-02-02 11:53:51 -0800

    my-launcher: show/hide keyboard

--- my-launcher/app/src/main/java/com/appmobiledeveloper/mylauncher/HomeActivity.java
+++ my-launcher/app/src/main/java/com/appmobiledeveloper/mylauncher/HomeActivity.java
@@ -2,20 +2,298 @@ package com.appmobiledeveloper.mylauncher;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.MotionEventCompat;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class HomeActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
+import static android.R.attr.data;
+
+public class HomeActivity extends Activity
+        implements GestureDetector.OnGestureListener
+{
+
+    public static final String DEBUG_TAG = "XXX";
+    private PackageManager manager;
+    private List<AppInfo> apps;
+
+
+    private GestureDetectorCompat mDetector;
+
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mDetector = new GestureDetectorCompat(this,this);
+
+        textView = (TextView)findViewById(R.id.search);
+
+
+        textView.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                // TODO Auto-generated method stub
+                String text = textView.getText().toString().toLowerCase(Locale.getDefault());
+//                adapter.filter(text);
+                if (text.equals("")){
+                    list.setVisibility(View.GONE);
+//                    hideSoftKeyboard(textView);
+                }else{
+                    list.setVisibility(View.VISIBLE);
+                }
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1,
+                                          int arg2, int arg3) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+                                      int arg3) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+
+        loadApps();
+        loadListView();
+        list.setVisibility(View.GONE);
+        textView.setVisibility(View.GONE);
+
+//        addClickListener();
+
+
+//        View myView = findViewById(R.id.activity_home);
+//        myView.setOnTouchListener(new View.OnTouchListener() {
+//            public boolean onTouch(View v, MotionEvent event) {
+//                // ... Respond to touch events
+//                int action = MotionEventCompat.getActionMasked(event);
+//
+//                switch(action) {
+//                    case (MotionEvent.ACTION_DOWN) :
+//                        Log.d(DEBUG_TAG,"Action was DOWN");
+//                        return true;
+//                    case (MotionEvent.ACTION_MOVE) :
+//                        Log.d(DEBUG_TAG,"Action was MOVE");
+//                        return true;
+//                    case (MotionEvent.ACTION_UP) :
+//                        Log.d(DEBUG_TAG,"Action was UP");
+//                        return true;
+//                    case (MotionEvent.ACTION_CANCEL) :
+//                        Log.d(DEBUG_TAG,"Action was CANCEL");
+//                        return true;
+//                    case (MotionEvent.ACTION_OUTSIDE) :
+//                        Log.d(DEBUG_TAG,"Movement occurred outside bounds " +
+//                                "of current screen element");
+//                        return true;
+//                    default :
+//                    return true;
+//                }
+//            }
+//        });
     }
+
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event){
+//
+//        int action = MotionEventCompat.getActionMasked(event);
+//
+//        switch(action) {
+//            case (MotionEvent.ACTION_DOWN) :
+//                Log.d(DEBUG_TAG,"Action was DOWN");
+//                return true;
+//            case (MotionEvent.ACTION_MOVE) :
+//                Log.d(DEBUG_TAG,"Action was MOVE");
+//                return true;
+//            case (MotionEvent.ACTION_UP) :
+//                Log.d(DEBUG_TAG,"Action was UP");
+//                return true;
+//            case (MotionEvent.ACTION_CANCEL) :
+//                Log.d(DEBUG_TAG,"Action was CANCEL");
+//                return true;
+//            case (MotionEvent.ACTION_OUTSIDE) :
+//                Log.d(DEBUG_TAG,"Movement occurred outside bounds " +
+//                        "of current screen element");
+//                return true;
+//            default :
+//                return super.onTouchEvent(event);
+//        }
+//    }
 
     public void openList(View v){
         Intent i = new Intent(this, AppListActivity.class);
         startActivity(i);
+    }
+    private void loadApps(){
+        manager = getPackageManager();
+        apps = new ArrayList<AppInfo >();
+
+        Intent i = new Intent(Intent.ACTION_MAIN, null);
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> availableActivities = manager.queryIntentActivities(i, 0);
+        for(ResolveInfo ri:availableActivities){
+            AppInfo app = new AppInfo();
+            app.title= ri.loadLabel(manager);
+            app.packageName= ri.activityInfo.packageName;
+            app.image= ri.activityInfo.loadIcon(manager);
+            apps.add(app);
+        }
+    }
+    private ListView list;
+    private void loadListView(){
+        list = (ListView)findViewById(R.id.search_listview);
+
+        ArrayAdapter<AppInfo> adapter = new ArrayAdapter<AppInfo>(this,
+                R.layout.list_item,
+                apps) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if(convertView == null){
+                    convertView = getLayoutInflater().inflate(R.layout.list_item, null);
+                }
+
+                ImageView appIcon = (ImageView)convertView.findViewById(R.id.item_app_icon);
+                appIcon.setImageDrawable(apps.get(position).image);
+
+                TextView appLabel = (TextView)convertView.findViewById(R.id.item_app_label);
+                appLabel.setText(apps.get(position).title);
+
+                TextView appName = (TextView)convertView.findViewById(R.id.item_app_name);
+                appName.setText(apps.get(position).packageName);
+
+                return convertView;
+            }
+        };
+
+        list.setAdapter(adapter);
+    }
+    private void addClickListener(){
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, int pos,
+                                    long id) {
+                Intent i = manager.getLaunchIntentForPackage(apps.get(pos).packageName.toString());
+                HomeActivity.this.startActivity(i);
+            }
+        });
+    }
+
+
+
+   /////
+    /////
+    /////
+    /////
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.mDetector.onTouchEvent(event);
+        // Be sure to call the superclass implementation
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+//        Log.d(DEBUG_TAG,"onDown: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+//        Log.d(DEBUG_TAG, "onFling: " + event1.toString()+event2.toString());
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+//        Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+                            float distanceY) {
+        Log.d(DEBUG_TAG, "onScroll: " + e1.toString()+e2.toString());
+        Toast.makeText(this, "OnScroll",
+                Toast.LENGTH_LONG).show();
+        textView.setVisibility(View.VISIBLE);
+        textView.requestFocus();
+        s();
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+//        Log.d(DEBUG_TAG, "onShowPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        h();
+        textView.setVisibility(View.GONE);
+//        hideKeyboard();
+//        hideSoftKeyboard(textView);
+//        Log.d(DEBUG_TAG, "onSingleTapUp: " + event.toString());
+        return true;
+    }
+
+//    @Override
+//    public boolean onDoubleTap(MotionEvent event) {
+//        Log.d(DEBUG_TAG, "onDoubleTap: " + event.toString());
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onDoubleTapEvent(MotionEvent event) {
+//        Log.d(DEBUG_TAG, "onDoubleTapEvent: " + event.toString());
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onSingleTapConfirmed(MotionEvent event) {
+//        Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.toString());
+//        return true;
+//    }
+
+
+    public void  s(){
+        InputMethodManager imm = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(textView, InputMethodManager.SHOW_IMPLICIT);
+    }
+
+    public void h(){
+        InputMethodManager imm = (InputMethodManager)
+        getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(textView.getWindowToken(), 0);
     }
 }
