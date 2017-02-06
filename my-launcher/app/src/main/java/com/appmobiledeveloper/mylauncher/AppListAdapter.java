a0eb90981a4e2525eeef5821445fcfbbb1fe8839
Mobile Developer <app.mobile.developer@gmail.com>
2017-02-06 00:44:33 -0800

    my-launcher: updated missed adapter for search

---
+++ my-launcher/app/src/main/java/com/appmobiledeveloper/mylauncher/AppListAdapter.java
@@ -0,0 +1,130 @@
+package com.appmobiledeveloper.mylauncher;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by grigorynovikov on 2/8/17.
+ */
+
+public class AppListAdapter  extends BaseAdapter {
+
+    // Declare Variables
+    Context mContext;
+    LayoutInflater inflater;
+    List<AppInfo> apps;
+    ArrayList<AppInfo> arrayList = null;
+    public AppListAdapter(Context context, List<AppInfo> appInfos) {
+        mContext = context;
+        this.apps = appInfos;
+        inflater = LayoutInflater.from(mContext);
+        arrayList  = new ArrayList<AppInfo>(appInfos);
+        arrayList.addAll(apps);
+    }
+
+    public class ViewHolder {
+        TextView label;
+        TextView name;
+    }
+
+    @Override
+    public int getCount() {
+        return apps.size();
+    }
+
+    @Override
+    public AppInfo getItem(int position) {
+        return apps.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(final int position, View view, ViewGroup parent) {
+
+
+        if(view == null){
+            view  = ((HomeActivity)mContext).getLayoutInflater().inflate(R.layout.list_item, null);
+        }
+
+        ImageView appIcon = (ImageView)view.findViewById(R.id.item_app_icon);
+        appIcon.setImageDrawable(apps.get(position).image);
+
+        TextView appLabel = (TextView)view.findViewById(R.id.item_app_label);
+        appLabel.setText(apps.get(position).title);
+
+        TextView appName = (TextView)view.findViewById(R.id.item_app_name);
+        appName.setText(apps.get(position).packageName);
+
+
+//        final ViewHolder holder;
+//        if (view == null) {
+//            holder = new ViewHolder();
+//            view = inflater.inflate(R.layout.list_item, null);
+//            // Locate the TextViews in listview_item.xml
+//            holder.label= (TextView) view.findViewById(R.id.item_app_label);
+//            holder.name= (TextView) view.findViewById(R.id.item_app_name);
+//            view.setTag(holder);
+//        } else {
+//            holder = (ViewHolder) view.getTag();
+//        }
+//        // Set the results into TextViews
+//        holder.label.setText(worldpopulationlist.get(position));
+//        holder.name.setText(worldpopulationlist.get(position).getCountry());
+//        holder.population.setText(worldpopulationlist.get(position).getPopulation());
+
+        // Listen for ListView Item Click
+        view.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // Send single item click data to SingleItemView Class
+                Intent intent = new Intent(mContext, AppInfo.class);
+                // Pass all data rank
+//                intent.putExtra("rank",(worldpopulationlist.get(position).getRank()));
+//                // Pass all data country
+//                intent.putExtra("country",(worldpopulationlist.get(position).getCountry()));
+//                // Pass all data population
+//                intent.putExtra("population",(worldpopulationlist.get(position).getPopulation()));
+                // Pass all data flag
+                // Start SingleItemView Class
+                mContext.startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+
+    // Filter Class
+    public void filter(String charText) {
+        charText = charText.toLowerCase(Locale.getDefault());
+        apps.clear();
+//        Log.d(DEBUG_TAG,"onDown: " + event.toString());
+        if (charText.length() == 0) {
+            apps.addAll(arrayList);
+        }
+        else
+        {
+            for (AppInfo ai: arrayList)
+            {
+                if (ai.title.toString().toLowerCase(Locale.getDefault()).contains(charText))
+                {
+                    apps.add(ai);
+                }
+            }
+        }
+        notifyDataSetChanged();
+    }
+}
